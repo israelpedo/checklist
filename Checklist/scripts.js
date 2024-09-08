44b3d854ca61644900c7fc9d09aef5b2551d1a5b
Israel Pedó <88521896+israelpedo@users.noreply.github.com>
2024-09-08 22:56:03 +0100

    Delete Checklist directory

--- Checklist/scripts.js
+++
@@ -1,8 +0,0 @@
-import { adicionarItem } from "./js/adicionarItem.js";
-import { verificarListaComprados } from "./js/verificarListaComprados.js";
-
-const botaoSalvarItem = document.getElementById("adicionar-item");
-botaoSalvarItem.addEventListener("click", adicionarItem);
-
-const listaComprados = document.getElementById("lista-comprados");
-verificarListaComprados(listaComprados);
